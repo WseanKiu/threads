59582f20fa7ab2041ba8a37c5511ac2d1cf2613f
Winston Neo <winstonseanqui@gmail.com>
2025-03-04 02:07:07 +0800

    View Comment threads

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -36,8 +36,25 @@ async function page ({ params }: { params: { id: string }}) {
       <div className='mt-7'>
         <Comment
           threadId={thread.id}
-          currentUserImage={user.imageUrl}
+          currentUserImage={userInfo.image}
           currentUserId={JSON.stringify(userInfo._id)} />
+      </div>
+
+      <div className='mt-10'>
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUser={childItem?.id || ''}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   )
