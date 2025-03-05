97927ffd728069d48818f2805982af2e700f3f60
Winston Neo <winstonseanqui@gmail.com>
2025-03-05 13:15:51 +0800

    Activities Page / Notification

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { profileTabs } from "@/constants";
-import Image from "next/image";
-import ThreadsTab from "@/components/shared/ThreadsTab";
 import UserCard from "@/components/cards/UserCard";
 
 
