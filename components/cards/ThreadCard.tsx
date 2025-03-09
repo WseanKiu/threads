1581a14fd4b4cb083dcae20a598ee5ae04ebc332
Winston Neo <winstonseanqui@gmail.com>
2025-03-09 15:05:00 +0800

    delete thread  and suggestions

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -2,11 +2,12 @@ import { formatDateString } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
+import DeleteThread from '../forms/DeleteThread';
 
 interface Props {
   key: string;
   id: string;
-  currentUser: string;
+  currentUserId: string;
   parentId: string | null;
   content: string;
   author: {
@@ -28,7 +29,7 @@ interface Props {
 
 const ThreadCard = ({
   id,
-  currentUser,
+  currentUserId,
   parentId,
   content,
   author,
@@ -108,9 +109,36 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+        <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        />
       </div>
-        {/* TODO: Delete thread */}
-        {/* TODO: Show thread profiles */}
+        {/* Show thread profiles */}
+        {!isComment && comments.length > 0 && (
+        <div className='ml-1 mt-3 flex items-center gap-2'>
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className='mt-1 text-subtle-medium text-gray-1'>
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )}
+
         {!isComment && community && (
           <Link href={`/communities/${community.id}`} className='mt-5 flex items-center'>
             <p className='text-subtle-medium text-gray-1'>
